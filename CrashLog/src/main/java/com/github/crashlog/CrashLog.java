9b6f58814966a5ce67bc7fff749221b8ede72b71
Administrator <zr2013123@sina.com>
2019-08-30 18:42:54 +0800

    file

--- CrashLog/src/main/java/com/github/crashlog/CrashLog.java
+++ CrashLog/src/main/java/com/github/crashlog/CrashLog.java
@@ -1,5 +1,6 @@
 package com.github.crashlog;
 
+import android.content.Context;
 import android.os.Environment;
 
 import java.io.File;
@@ -8,24 +9,38 @@ import java.io.File;
  *   created by android on 2019/8/27
  */
 public class CrashLog implements Thread.UncaughtExceptionHandler {
-    private String rootPath=Environment.getExternalStorageDirectory().getAbsolutePath()+File.separator+"crash";
-    private String nameSuffix=".log";
+    private String rootPath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "crash";
+    private String nameSuffix = ".log";
+    private Context context;
+    private Thread.UncaughtExceptionHandler uncaughtException;
     private static CrashLog singleObj;
-        private CrashLog() {
+    private CrashLog() {
     }
-    public static CrashLog get(){
-        if(singleObj==null){
-            synchronized (CrashLog.class){
-                if(singleObj==null){
-                    singleObj=new CrashLog();
+
+    public static CrashLog get() {
+        if (singleObj == null) {
+            synchronized (CrashLog.class) {
+                if (singleObj == null) {
+                    singleObj = new CrashLog();
                 }
             }
         }
         return singleObj;
     }
 
-    @Override
+    public void init(Context context) {
+        this.context = context;
+        if (this.uncaughtException == null) {
+            this.uncaughtException = Thread.getDefaultUncaughtExceptionHandler();
+            Thread.setDefaultUncaughtExceptionHandler(this);
+        }
+    }
+
     public void uncaughtException(Thread t, Throwable e) {
+        ExceptionAnalysis.getInstance().saveCrashInfo(this.context, e, true);
+        if (this.uncaughtException!=null&&this.uncaughtException.equals(this)==false) {
+            this.uncaughtException.uncaughtException(t, e);
+        }
 
     }
 }
